01a7828d4f13df6894247501a535214e3d9e2cb0
moin <moinur.hossain@yahoo.com>
2024-11-05 11:58:04 +0600

    2nd Modification

--- src/HelloIntelliJ.java
+++ src/HelloIntelliJ.java
@@ -1,6 +1,6 @@
 public class HelloIntelliJ {
     public static void main(String[] args) {
-        System.out.println("Hello IntelliJ! This is Moin from BD");
+        System.out.println("Hello IntelliJ! This is Moin");
     }
 }
 
