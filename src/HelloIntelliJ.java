11d9c90f5af811babf9357521386c0b0255cdb81
moin <moinur.hossain@yahoo.com>
2024-11-05 12:45:07 +0600

    Add input parameter to HelloIntelliJ program

--- src/HelloIntelliJ.java
+++ src/HelloIntelliJ.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class HelloIntelliJ {
     public static void main(String[] args) {
-        System.out.println("Hello IntelliJ! This is Moin");
-    }
-}
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your Name:");
+        String message = scanner.nextLine();
+        // add line to display message
+// add line to close scanner }}
+        scanner.close();
+        System.out.println(message);
+    }}
 
